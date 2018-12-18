330e435eabd1f214e219c5b0e4038643b8d476d7
Nicat Guliyev <ng@nicats-mbp.mshome.net>
2018-12-18 16:03:12 +0400

    Initial Commit

---
+++ EMDK/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  EMDK
+//
+//  Created by Nicat Guliyev on 12/18/18.
+//  Copyright © 2018 Nicat Guliyev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
